bfc77ba926974e57939f8b22a6c5077dc22d8bd0
SvetaGrigoreva <130225058+SvetaGrigoreva@users.noreply.github.com>
2023-04-12 09:12:44 +0000

    secondary buttom changed

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -5,7 +5,7 @@ function Addresses() {
     return (
         <div className='d-inline-flex flex-column col justify-content-between p-4 border rounded-3 ms-5'> 
             <div className='title'>Адреса заведений</div>
-            <div className='d-inline-flex flex-column gap-4'>
+            <div className='d-flex flex-column gap-5'>
                 <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
                 <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
                 <PrimaryButton>Добавить филиал</PrimaryButton>
